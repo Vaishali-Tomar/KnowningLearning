1b1fb1c020dab2adf2a79e79223fb914730694ee
Vaishali Tomar <vaishalitomar830@gmail.com>
2024-10-12 20:41:16 +0530

    clr

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -13,31 +13,30 @@ function Signup() {
       navigate('/login');
     } else {
       alert('Please enter both username and password.');
-
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-green-400 to-blue-500">
-      <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
-        <h2 className="text-3xl font-semibold text-center mb-6 text-gray-700">Signup</h2>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-bl from-indigo-500 to-pink-500">
+      <div className="bg-white shadow-2xl rounded-lg p-10 max-w-sm w-full">
+        <h2 className="text-4xl font-bold text-center mb-8 text-gray-800">Create Account</h2>
         <input
           type="text"
           placeholder="Username"
           value={userData.username}
           onChange={(e) => setUserData({ ...userData, username: e.target.value })}
-          className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full px-4 py-3 mb-5 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
         <input
           type="password"
           placeholder="Password"
           value={userData.password}
           onChange={(e) => setUserData({ ...userData, password: e.target.value })}
-          className="w-full px-4 py-2 mb-6 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+          className="w-full px-4 py-3 mb-6 border-2 border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
         />
         <button
           onClick={handleSignup}
-          className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg transition duration-300"
+          className="w-full bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-semibold py-3 px-4 rounded-lg transition duration-300 transform hover:scale-105"
         >
           Signup
         </button>
